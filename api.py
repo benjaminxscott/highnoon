13319c12b937273b1f891a8a867db41f48acaef7
Ben <bschmoker+c9@gmail.com>
2016-07-11 01:43:08 +0000

    refactor

--- api.py
+++ api.py
@@ -19,15 +19,15 @@ from models import Player, PlayerMessage, Game, GameMessage
 from models import StringMessage
 from utils import get_by_urlsafe, uniq_id
 
-USER_REQUEST = endpoints.ResourceContainer(PlayerMessage)
+PLAYER_REQUEST = endpoints.ResourceContainer(PlayerMessage)
 GAME_REQUEST = endpoints.ResourceContainer(GameMessage)
 GAME_LOOKUP_REQUEST = endpoints.ResourceContainer(game_id=messages.StringField(1, required=True ) )
 
 @endpoints.api(name='aceofblades', version='v1')
 class AceofBlades(remote.Service):
     """Game API"""
-    @endpoints.method(request_message=USER_REQUEST,
-                      response_message=StringMessage,
+    @endpoints.method(request_message=PLAYER_REQUEST,
+                      response_message=PlayerMessage,
                       path='player/new',
                       name='create_player',
                       http_method='POST' )
@@ -40,15 +40,14 @@ class AceofBlades(remote.Service):
             
         player_id = uniq_id()
         
-        user = Player(player_id = player_id, player_name=player_name)
-        user.put()
+        player = Player(player_id = player_id, player_name=player_name)
+        player.put()
         
         # TODO return id and name as json instead
-        return StringMessage(message='Player {} with ID {} has stepped through the door'.format(
-                player_name, player_id))
+        return player.to_message()
 
     @endpoints.method(request_message=GAME_REQUEST,
-                      response_message=StringMessage,
+                      response_message=GameMessage,
                       path='game/new',
                       name='create_game',
                       http_method='POST' )
@@ -66,8 +65,7 @@ class AceofBlades(remote.Service):
         game = Game(game_id = game_id, slinger = slinger.key, status = "waiting")
         game.put()
         
-        return StringMessage(message='Game with ID {} for player ID {} and player name {} '.format(
-                game_id, slinger.player_id, slinger.player_name))
+        return game.to_message()
                 
     
     @endpoints.method(request_message=GAME_LOOKUP_REQUEST,
--- models.py
+++ models.py
@@ -7,28 +7,46 @@ from datetime import date
 from protorpc import messages
 from google.appengine.ext import ndb
 
+# ASK: possible to use obj.key rather than storing explicit ID? 
+# ran into issue trying to query with just the int ID and not , although seemed cleaner to let the DB keep track of primary key and return obj.key.id() 
+
 # --- Request parameters ---
 class PlayerMessage(messages.Message):
     """Used to create a new player"""
     desired_name = messages.StringField(1)
+    player_id = messages.StringField(2)
+    player_name = messages.StringField(3)
     
 class GameMessage(messages.Message):
     """Used to create a new game"""
     player_id = messages.StringField(1, required=True)
     rival_id = messages.StringField(2)
+    game_id = messages.StringField(3)
+    status = messages.StringField(4)
 
 # --- Data Model ---
 class Player(ndb.Model):
     """User profile"""
     player_id = ndb.StringProperty(required=True)
     player_name =ndb.StringProperty()
-
+    
+    def to_message(self):
+        return PlayerMessage(player_id=self.player_id, player_name= self.player_name)
+       
 class Game(ndb.Model):
     """Game object"""
     game_id = ndb.StringProperty(required=True)
     slinger = ndb.KeyProperty(required=True, kind='Player')
-    rival = ndb.KeyProperty(kind='Player', default=None)
     status = ndb.StringProperty(required=True, default="waiting")
+    rival = ndb.KeyProperty(kind='Player', default=None)
+    
+    def to_message(self):
+        return GameMessage(
+          game_id = self.game_id, 
+        # slinger is primary key into Player DB
+          player_id = Player.query(Player.key == self.slinger).get().player_id ,
+#TODO          rival_id = Player.query(Player.key == self.rival).get().player_id ,
+          status = self.status)
 
     @classmethod
 
